ceb6d4b7587f8d64ef2e7b0d91cd49af802cc81e
Christophe28 <christophebuffe@gmail.com>
2021-11-30 11:42:33 +0100

    sanitize and del + new name file

--- Model/People/People.php
+++ Model/People/People.php
@@ -3,7 +3,7 @@
 	// request phpMyAdmin
 	try{
 		// Connect to mysql
-		$bdd = new PDO('mysql:host=127.0.0.1;dbname=Copig;charset=utf8', 'kriss', 'Sommation01');
+		$bdd = new PDO('mysql:host=127.0.0.1;dbname=Copig;charset=utf8', 'kriss', '');
 	}
 	
 	catch(Exception $e){
